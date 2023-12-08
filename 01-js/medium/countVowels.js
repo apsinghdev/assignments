9f13686147c97159eb7656451e2b1612984363ea
ajeet <109718740+ajeetcode@users.noreply.github.com>
2023-12-08 16:37:12 +0530

    builds Count Vowel function. Assignments-med-1

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,31 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  str = str.toLowerCase();
+  let charsInString = str.split("");
+  let vowelCount = 0;
+  for (let i = 0; i <= charsInString.length - 1; i++) {
+    let vowels = ["a", "e", "i", "o", "u"];
+    if (vowels.includes(charsInString[i])) {
+      vowelCount++;
+    }
+  }
+  return vowelCount;
 }
 
+console.log(countVowels("ajeet146442798298....aoeoeoeocbccbmq popoei"));
 module.exports = countVowels;
+
+/* 
+
+1. convert chars of string into lowercase
+2. split the string into each char ( in array)
+3. initialise a var that counts vowel
+4. start a loop that iterates whole array
+5. check if arr[i] is a vowel
+6. if yes, increase count by one
+7. if no, pass
+8. finally return the total count of vowels
+
+
+*/
