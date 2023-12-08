60a9651fd5a62c0978f0fd6328e3e0f27d8b4235
ajeet <109718740+ajeetcode@users.noreply.github.com>
2023-12-09 01:50:40 +0530

    creates script that checks time taken for execution of code in js [assignment-med-3]

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,6 +8,19 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
+
 function calculateTime(n) {
-    return 0.01;
+    let sum = 0;
+    // previously i was using date.getTime(), and it was giving me the same time for each start and end. why? because i was creating only one Date obj and was using it for both timestamp. so used seprate Date obj for each time timestamp to get the latest time 
+    let startTime = new Date().getTime();
+    console.log(startTime);
+    for(i=1; i<=n; i++){
+        sum = sum+i;
+    }
+    let endTime = new Date().getTime();
+    console.log(endTime);
+    let time = (endTime-startTime)/1000;
+    return `${time} seconds`
 }
+
+console.log(calculateTime(10000000000));
