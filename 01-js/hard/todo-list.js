33385fac3e583e6a110bb5a2d7b169e7d2e554f3
ajeet <109718740+ajeetcode@users.noreply.github.com>
2023-12-08 16:39:50 +0530

    builds Todo-list. [assignments-hard-2]

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,40 @@
 */
 
 class Todo {
-
+  constructor() {
+    this.todolist = [];
+  }
+  add(todo) {
+    this.todolist.push(todo);
+  }
+  remove(indexOfTodo) {
+    this.todolist.splice(indexOfTodo, 1);
+  }
+  update(index, updatedTodo) {
+    if (!(index >= 0 && index < this.todolist.length)) {
+      return null;
+    }
+    this.todolist[index] = updatedTodo;
+  }
+  getAll() {
+    return this.todolist;
+  }
+  get(indexOfTodo) {
+    // before writing logic in if, clearly define what i want the param to be. also make use of example
+    if (!(indexOfTodo >= 0 && indexOfTodo < this.todolist.length)) {
+      return null;
+    }
+    return this.todolist[indexOfTodo];
+  }
+  clear() {
+    this.todolist = [];
+  }
 }
-
 module.exports = Todo;
+var myTodo = new Todo();
+myTodo.add("workout", "task2");
+myTodo.add("task2");
+myTodo.update(1, "updated task2");
+console.log(myTodo.getAll());
+myTodo.get(0);
+console.log(myTodo.getAll());
