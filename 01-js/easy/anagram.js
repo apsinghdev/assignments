34d78b1a858ffe9326c05d509b7fd086f4e679aa
ajeet <109718740+ajeetcode@users.noreply.github.com>
2023-12-09 11:02:18 +0530

    Creates Script that checks an Anagram. [Assignment-easy-1]

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,23 @@
 */
 
 function isAnagram(str1, str2) {
-
+  let result = true;
+  let Arr1 = str1.split("");
+  let Arr2 = str2.split("");
+  // write a logic to return false if length of the strings is not same
+  if (!(Arr1.length === Arr2.length)) {
+    result = false;
+  } else {
+    for (let i = 0; i < Arr2.length; i++) {
+      if (!Arr1.includes(Arr2[i])) {
+        result = false;
+      } else {
+        result = true;
+      }
+    }
+  }
+  return result;
 }
 
+console.log(isAnagram("abc", "bcaa"));
 module.exports = isAnagram;
