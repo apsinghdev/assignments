e12f8ae502381f817853ff81d831b3a3aaad40f1
ajeet <109718740+ajeetcode@users.noreply.github.com>
2023-12-09 16:28:10 +0530

    creates script to find the largest element in an array

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,18 @@
 */
 
 function findLargestElement(numbers) {
+    var largestNum = numbers[0];
+    if(numbers.length === 0){
+        largestNum = undefined;
+    } else{
+       for (let i = 0; i < numbers.length; i++) {
+         if (numbers[i] > largestNum) {
+           largestNum = numbers[i];
+         }
+       }
+    }
     
+    return largestNum;
 }
-
+console.log(findLargestElement([-3.5, -7.2, -2.1, -9.8, -1.9]));
 module.exports = findLargestElement;
