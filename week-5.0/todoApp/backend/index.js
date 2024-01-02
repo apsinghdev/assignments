a96c0e2d201df323f8f9e9c7a5c1c28e7e2555d0
apsinghdev <ajeetpratap517@gmail.com>
2024-01-02 14:44:11 +0530

    format code

--- week-5.0/todoApp/backend/index.js
+++ week-5.0/todoApp/backend/index.js
@@ -1,22 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 4000;
-const {createTodo, updateTodo} = require('./types');
+const { createTodo, updateTodo } = require("./types");
 
 app.use(express.json());
 
-function validateUserData(req,res,next){
-    try{
-        createTodo.safeParse(req.body);
-        next();
-    } catch(error){
-        res.status(400).json({error: "inputs are not correct"});
-    }
+function validateUserData(req, res, next) {
+  try {
+    createTodo.safeParse(req.body);
+    next();
+  } catch (error) {
+    res.status(400).json({ error: "inputs are not correct" });
+  }
 }
 
-app.post("/todo", validateUserData, (req, res) => {
-  
-});
+app.post("/todo", validateUserData, (req, res) => {});
 
 app.get("/todos", (req, res) => {});
 
