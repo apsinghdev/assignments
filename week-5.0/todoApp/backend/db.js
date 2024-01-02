e8ff7e9b57610b7ea8f6f9c130588eb334a21b06
apsinghdev <ajeetpratap517@gmail.com>
2024-01-02 15:29:40 +0530

    add and initialize mongodb for the project

---
+++ week-5.0/todoApp/backend/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const { boolean } = require('zod');
+const {Schema} = mongoose;
+mongoose.connect(
+  "mongodb+srv://ajeet:ajeet@mern-estate.euetct6.mongodb.net/?retryWrites=true&w=majority"
+);
+
