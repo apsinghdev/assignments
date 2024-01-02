e3801ae60feceb00b64338a77cedca29b3c9e1be
apsinghdev <ajeetpratap517@gmail.com>
2024-01-02 15:30:45 +0530

    create todo schema and model

--- week-5.0/todoApp/backend/db.js
+++ week-5.0/todoApp/backend/db.js
@@ -1,7 +1,14 @@
-const mongoose = require('mongoose');
-const { boolean } = require('zod');
-const {Schema} = mongoose;
+const mongoose = require("mongoose");
+const { boolean } = require("zod");
+const { Schema } = mongoose;
 mongoose.connect(
   "mongodb+srv://ajeet:ajeet@mern-estate.euetct6.mongodb.net/?retryWrites=true&w=majority"
 );
 
+const todoSchema = new Schema({
+  title: String,
+  description: String,
+  completed: boolean,
+});
+
+const todo = mongoose.model("todo", todoSchema);
