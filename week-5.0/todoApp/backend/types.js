0e24ef548cb1ec0534d3c24e2ba167369c159e4d
apsinghdev <ajeetpratap517@gmail.com>
2024-01-02 14:09:32 +0530

    create input validation schema using zod

--- week-5.0/todoApp/backend/types.js
+++ week-5.0/todoApp/backend/types.js
@@ -1,1 +1,17 @@
 const zod = require('zod');
+
+const createTodoSchema = zod.object({
+    title: zod.string(),
+    description: zod.string(),
+})
+
+const updateTodoSchema = zod.object({
+    id: zod.string()
+})
+
+// export the schemas
+
+module.exports = {
+    createTodo: createTodoSchema,
+    updateTodo: updateTodoSchema
+}
