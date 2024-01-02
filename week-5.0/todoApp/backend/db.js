6b00710ad17c482c1b65646ea99d86bea82337f3
apsinghdev <ajeetpratap517@gmail.com>
2024-01-02 15:33:16 +0530

    fix mistake of zod

--- week-5.0/todoApp/backend/db.js
+++ week-5.0/todoApp/backend/db.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const { boolean } = require("zod");
 const { Schema } = mongoose;
 mongoose.connect(
   "mongodb+srv://ajeet:ajeet@mern-estate.euetct6.mongodb.net/?retryWrites=true&w=majority"
@@ -8,7 +7,7 @@ mongoose.connect(
 const todoSchema = new Schema({
   title: String,
   description: String,
-  completed: boolean,
+  completed: Boolean,
 });
 
 const todo = mongoose.model("todo", todoSchema);
