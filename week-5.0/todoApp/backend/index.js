cef24193183de2f5508c048f113431fc9eacb053
apsinghdev <ajeetpratap517@gmail.com>
2024-01-02 14:37:49 +0530

    apply input validation to the post route

--- week-5.0/todoApp/backend/index.js
+++ week-5.0/todoApp/backend/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const {createTodo, updateTodo} = require('./types');
 
 app.use(express.json());
 
-app.post("/todo", (req, res) => {});
+function validateUserData(req,res,next){
+    try{
+        createTodo.safeParse(req.body);
+        next();
+    } catch(error){
+        res.status(400).json({error: "inputs are not correct"});
+    }
+}
+
+app.post("/todo", validateUserData, (req, res) => {
+  
+});
 
 app.get("/todos", (req, res) => {});
 
