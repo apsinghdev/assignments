0c81598860fd10a17bdbb67878f51a6369f4ecbd
apsinghdev <ajeetpratap517@gmail.com>
2024-01-02 13:39:38 +0530

    add required routes for app

--- week-5.0/todoApp/backend/index.js
+++ week-5.0/todoApp/backend/index.js
@@ -4,4 +4,11 @@ const port = 4000;
 
 app.use(express.json());
 
+app.post("/todo", (req, res) => {});
+
+app.get("/todos", (req, res) => {});
+
+app.put("/completed", (req, res) => {});
+
+
 app.listen(`Server is running on ${port}`);
