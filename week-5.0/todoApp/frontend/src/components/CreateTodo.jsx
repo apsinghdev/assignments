7500164ce11cd7e761ef21f44c705ed7f5d578ca
apsinghdev <ajeetpratap517@gmail.com>
2024-01-03 13:09:25 +0530

    create a react component for todoapp

---
+++ week-5.0/todoApp/frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,11 @@
+export function CreateTodo(){
+    return (
+      <>
+        <input type="text" placeholder="title"></input>
+        <br></br>
+        <input type="text" placeholder="description"></input>
+        <br></br>
+        <button>Add todo</button>
+      </>
+    );
+}
