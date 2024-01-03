f5653d7d79b218561585094082679c5378727e20
apsinghdev <ajeetpratap517@gmail.com>
2024-01-03 20:38:24 +0530

    create logic to add todos to the database

--- week-5.0/todoApp/frontend/src/components/CreateTodo.jsx
+++ week-5.0/todoApp/frontend/src/components/CreateTodo.jsx
@@ -1,13 +1,49 @@
-// this component creates the todos 
+// this component creates the todos
+import { useState } from "react";
+export function CreateTodo() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-export function CreateTodo(){
-    return (
-      <>
-        <input style={{padding: 10, margin: 10}} type="text" placeholder="title"></input>
-        <br></br>
-        <input style={{padding: 10, margin: 10}} type="text" placeholder="description"></input>
-        <br></br>
-        <button style={{padding: 10, margin: 10}}>Add todo</button>
-      </>
-    );
+  function clickHandler() {
+    fetch("http://localhost:4000/todo", {
+      method: "POST",
+      body: JSON.stringify({
+        title: title,
+        description: description,
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    }).then(async (data) => {
+      const response = data.json();
+      alert("todo added");
+    });
+  }
+  return (
+    <>
+      <input
+        style={{ padding: 10, margin: 10 }}
+        type="text"
+        placeholder="title"
+        onChange={function (e) {
+          const titleValue = e.target.value;
+          setTitle(titleValue);
+        }}
+      ></input>
+      <br></br>
+      <input
+        style={{ padding: 10, margin: 10 }}
+        type="text"
+        placeholder="description"
+        onChange={function (e) {
+          const descValue = e.target.value;
+          setDescription(descValue);
+        }}
+      ></input>
+      <br></br>
+      <button onClick={clickHandler} style={{ padding: 10, margin: 10 }}>
+        Add todo
+      </button>
+    </>
+  );
 }
