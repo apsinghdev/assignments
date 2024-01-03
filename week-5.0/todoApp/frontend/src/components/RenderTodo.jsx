56ecfe7b1f5d65e6b8d91e87af756a3d89420dd3
apsinghdev <ajeetpratap517@gmail.com>
2024-01-03 20:40:43 +0530

    write logic to update the todo

--- week-5.0/todoApp/frontend/src/components/RenderTodo.jsx
+++ week-5.0/todoApp/frontend/src/components/RenderTodo.jsx
@@ -1,17 +1,39 @@
 // this component renders the the todos on screen
+import { useState } from "react";
 
-export function RenderTodo({todos}) {
+export function RenderTodo({ todos }) {
+  function markCompleteHandler(todoId) {
+    fetch("http://localhost:4000/completed", {
+      method: "PUT",
+      body: JSON.stringify({
+        id: todoId,
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    }).then(async (data) => {
+      const res = await data.json();
+      alert(res.msg);
+    });
+  }
   return (
     <>
-     {todos.map((todo)=>{
+      {todos.map((todo) => {
         return (
-          <>
+          <div key={todo._id}>
             <h4>{todo.title}</h4>
             <h4>{todo.description}</h4>
-            <button>{todo.completed == true ? 'completed':'Mark as complete'}</button>
-          </>
+            <button
+              onClick={() => {
+                markCompleteHandler(todo._id);
+              }}
+              id={todo._id}
+            >
+              {todo.completed ? "Completed" : "Mark as completed"}
+            </button>
+          </div>
         );
-     })}
+      })}
     </>
   );
 }
