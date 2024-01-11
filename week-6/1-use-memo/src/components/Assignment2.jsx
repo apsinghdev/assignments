840eb54f077be468ee58a43cd0a19ef61bfdd082
apsinghdev <ajeetpratap517@gmail.com>
2024-01-11 22:26:50 +0530

    solve assignment-2 of use-memo

--- week-6/1-use-memo/src/components/Assignment2.jsx
+++ week-6/1-use-memo/src/components/Assignment2.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+/* eslint-disable react/jsx-key */
+import React, { useEffect, useState, useMemo } from "react";
 
 // In this assignment, you will create a component that renders a large list of sentences and includes an input field for filtering these items. 
 // The goal is to use useMemo to optimize the filtering process, ensuring the list is only re-calculated when necessary (e.g., when the filter criteria changes).
@@ -17,17 +18,26 @@ for (let i = 0; i < TOTAL_LINES; i++) {
 }
 
 export function Assignment2() {
-    const [sentences, setSentences] = useState(ALL_WORDS);
-    const [filter, setFilter] = useState("");
+  const [sentences, setSentences] = useState(ALL_WORDS);
+  const [filter, setFilter] = useState("");
 
-    const filteredSentences = sentences.filter(x => x.includes(filter))
+  // eslint-disable-next-line react/display-name
+  //  It's good practice to include only the dependencies that are directly used in the memoized function and are subject to change during the component's lifecycle.
+  const filteredSentences = useMemo(() => {
+    return sentences.filter((x) => x.includes(filter));
+  }, [sentences,filter]);
 
-    return <div>
-        <input type="text" onChange={(e) => {
-            setFilter(e.target.value)
-        }}></input>
-        {filteredSentences.map(word => <div>
-            {word}    
-        </div>)}
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => {
+          setFilter(e.target.value);
+        }}
+      ></input>
+      {filteredSentences.map((word) => (
+        <div>{word}</div>
+      ))}
     </div>
+  );
 }
