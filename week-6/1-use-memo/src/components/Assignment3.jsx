68ad289a47eb8c112580fe1436e2dcaca8ace73e
apsinghdev <ajeetpratap517@gmail.com>
2024-01-11 22:58:31 +0530

    solve assignment 3 in use memo

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -8,11 +8,21 @@ const Assignment3 = () => {
         { name: 'Chips', value: 20 },
         { name: 'Onion', value: 30 },
         { name: 'Tomato', value: 30 },
+        { name: 'Tomato', value: 30 },
+        { name: 'Tomato', value: 30 },
+        { name: 'Tomato', value: 30 },
+        { name: 'Tomato', value: 30 },
+        { name: 'Tomato', value: 30 },
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(() => {
+      let total = 0;
+      return items.reduce((accumulator, data) => {
+        return accumulator+data.value;
+      },total);
+    }, [items]);
     // Your code ends here
     return (
         <div>
