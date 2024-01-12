c3aaaa4292fcc9ec8d30bb477ab3edec5490b995
apsinghdev <ajeetpratap517@gmail.com>
2024-01-12 20:49:53 +0530

    solve assin-1 of useref

--- week-6/3-use-ref/src/components/Assignment1.jsx
+++ week-6/3-use-ref/src/components/Assignment1.jsx
@@ -1,20 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 // Create a component with a text input field and a button. When the component mounts or the button is clicked, automatically focus the text input field using useRef.
 
 export function Assignment1() {
-
+const inputRef = useRef();
+// useEffect will be executed when the component will mount
+// why using useEffect? - coz we dont read and write ref directly in the component
+// its given an empty dependency arr so that it executes only one in first render
     useEffect(() => {
-
+        inputRef.current.focus();
     }, []);
-
+// second condition was to focus when the button is clicked
     const handleButtonClick = () => {
-
+        inputRef.current.focus();
     };
 
     return (
         <div>
-            <input type="text" placeholder="Enter text here" />
+            {/* in order to set ref of an dom elem we use ref attribute */}
+            <input ref={inputRef} type="text" placeholder="Enter text here" />
             <button onClick={handleButtonClick}>Focus Input</button>
         </div>
     );
